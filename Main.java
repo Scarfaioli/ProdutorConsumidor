ae7938b6fa73003c5e4306907b26488e1dc7f2ce
Samuel Faioli <scarf1999@gmail.com>
2023-11-21 17:55:48 -0300

    .

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-import java.util.LinkedList;
-
 public class Main{
     public static void main(String[] args) {
         int n=1, m=1;
@@ -8,7 +6,11 @@ public class Main{
         Thread[] consumidores = new Thread[m];
         for (int i = 0; i < consumidores.length; i++) {
             Consumidor consumidor = new Consumidor(noticias);
-            consumidores[i] = new Thread(consumidor, "Thread "+i);
+            consumidores[i] = new Thread(consumidor, "Consumidor "+i);
+        }
+        for (int i = 0; i < produtores.length; i++) {
+            Produtor produtor = new Produtor(noticias);
+            consumidores[i] = new Thread(produtor, "Produtor "+i);
         }
 
     }
